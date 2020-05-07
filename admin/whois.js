7a04fc68af376cc2f21852a94ac2bce6aa0d1877
astalios <37106010+Astalios@users.noreply.github.com>
2020-05-08 00:24:43 +0200

    Update whois.js

--- admin/whois.js
+++ admin/whois.js
@@ -19,11 +19,11 @@ module.exports = {
   	fields: [
   		{
   			name: 'field 1',
-  			value: 'lorem ipsum',
+  			value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   		},
   		{
-  			name: '\u200b',
-  			value: '\u200b',
+  			name: ' field 1.1 bien voyons monsieur l_arbitre',
+  			value: "oui c'est quoi ça ",
   			inline: false,
   		},
       {
@@ -31,8 +31,8 @@ module.exports = {
   			value: 'lorem ipsum ooc',
   		},
   		{
-  			name: '\u200b',
-  			value: '\u200b',
+  			name: 'field 2.2',
+  			value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   			inline: false,
   		},
   		{
