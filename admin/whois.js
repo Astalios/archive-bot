bb96dc558128d7136f1a57184b98127596daf83c
astalios <37106010+Astalios@users.noreply.github.com>
2020-05-08 18:27:39 +0200

    Update whois.js

--- admin/whois.js
+++ admin/whois.js
@@ -26,7 +26,7 @@ module.exports = {
       msg.channel.send("Syntax Error, use this : `a!about <@user]>`");
     }
 
-    rolesList = whoamiUserMention.roles.map(usrRoles => usrRoles}).join('')};
+    rolesList = whoamiUserMention.roles.map(usrRoles => usrRoles).join('');
     rolesColor = `${msg.guild.member(whoamiUserMention).displayHexColor}`;
     whoamiUserJoinedDate = `${msg.guild.member(whoamiUserMention).joinedAt.toDateString()}`;
     whoamiUserName = `${whoamiUserMention.username}`;
