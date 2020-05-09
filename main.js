39b233661af3851e008497eadb67c91dce1396de
astalios <37106010+Astalios@users.noreply.github.com>
2020-05-09 15:53:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -61,12 +61,7 @@ client.on('message', msg => {
         break;
       //ty kouine for this command
       case "cbt":
-        const msgCBT = "cock and ball torture for you ";
-        if(msg.mentions.users.size != 1){
-          msg.channel.send("Syntax Error, use this : `a!cbt [@user]`");
-        } else {
-          msg.channel.send(msgCBT + '<@'+msg.mentions.users.first()+'>' );
-        }
+				client.funCommands.get('cbt').execute(msg, args);
         break;
       // ABOUT ME, ABOUT YOU
       case "whois":
