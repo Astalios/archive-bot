13094fa77ed704f1b72c4774201b373403f7a56b
astalios <37106010+Astalios@users.noreply.github.com>
2020-05-09 19:46:39 +0200

    test embed pour nitro bar
    oui

---
+++ core/admin/testembed.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: 'testembed',
+  description: 'description',
+  execute(msg,args) {
+
+    let nitrobar = "[==========1️⃣==========2️⃣==>       3️⃣]" ;
+    const testembed = {
+      fields: [
+    		{
+    			name: 'NITRO TIER - BOOSTERS' + `LOREM IPSUM`,
+          value: `${nitrobar}`,
+          inline: false,
+    		},
+    };
+    msg.channel.send({ embed: testembed });
+  },
+};
