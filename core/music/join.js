95ad460f5aebb940a2e86a0dcfe3c6abb3a782dc
astalios <37106010+Astalios@users.noreply.github.com>
2020-12-04 23:08:24 +0100

    Update join.js

--- core/music/join.js
+++ core/music/join.js
@@ -4,7 +4,7 @@ module.exports = {
   execute(msg,args) {
     // joining a voice channel if the person calling it is inside, otherwise throw error
     if (msg.member.voice.channel){
-      const connect = await msg.member.voice.channel.join();
+          msg.member.voice.channel.join();
     } else {
       msg.reply('Merci de rejoindre un channel vocal avant !');
     }
