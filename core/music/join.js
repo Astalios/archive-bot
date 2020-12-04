4d951067a949c66f9acd6dd470401b11515ae8a7
astalios <37106010+Astalios@users.noreply.github.com>
2020-12-05 00:14:38 +0100

    creating leave.js, mod join.js

--- core/music/join.js
+++ core/music/join.js
@@ -3,8 +3,9 @@ module.exports = {
   description: 'Join a voice channel',
   execute(msg,args) {
     // joining a voice channel if the person calling it is inside, otherwise throw error
+    // I need to secure the connexion to the voice channel : if it joins a channel that he can't have access, crash the bot
     if (msg.member.voice.channel){
-          msg.member.voice.channel.join();
+          const connection = msg.member.voice.channel.join();
     } else {
       msg.reply('Merci de rejoindre un channel vocal avant !');
     }
