7c54e0872db0bf759e2c456b809ef2f644085005
astalios <37106010+Astalios@users.noreply.github.com>
2020-12-04 16:27:14 +0100

    mise en place module musique

---
+++ core/music/join.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'join',
+  description: 'Join a voice channel',
+  execute(msg,args) {
+    // joining a voice channel if the person calling it is inside, otherwise throw error
+    if (msg.member.voice.channel){
+      const connect = await msg.member.voice.channel.join();
+    } else {
+      msg.reply('Merci de rejoindre un channel vocal avant !');
+    }
+  },
+};
