3fb863e17a84fdad35aac64a382230bfe37b95c5
astalios <37106010+Astalios@users.noreply.github.com>
2020-12-05 00:48:06 +0100

    modding serverInfo + trying ot fix leave

--- core/admin/serverinfo.js
+++ core/admin/serverinfo.js
@@ -14,6 +14,7 @@ module.exports = {
     let serverCreationDate;
     let serverMembersCount;
     let serverRolesCount;
+    let serverVerificationLevel;
     let serverID;
     let serverRegion;
     let serverBoosterCount;
--- core/music/leave.js
+++ core/music/leave.js
@@ -2,6 +2,8 @@ module.exports = {
   name: 'leave',
   description: 'leave the voice channel',
   execute(msg,args) {
+
+    const Discord = require('discord.js');
     // leaving the voice channel if the person is inside the same channel as the bot is
     // I need to secure the connexion to the voice channel : if it joins a channel that he can't have access, crash the bot
     if (msg.member.voice.channel == client.voice.channel){
